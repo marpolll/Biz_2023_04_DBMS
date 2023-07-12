dc1535290afff2050e4b0536f1bd1be8969b1955
marpolll <minstarsoso@gmail.com>
2023-07-12 16:42:05 +0900

    test

--- TodoList/root(2023-07-06).sql
+++ TodoList/root(2023-07-06).sql
@@ -84,13 +84,151 @@ insert into calendar(id, events) values(2, '["책읽기", "2023-07-16", "2023-07
 
 SELECT * FROM calendar;
  
+ CREATE DATABASE scoreDB;
+ 
+ use scoreDB;
  
  
+ CREATE USER 'korea'@'localhost'
+identified BY '!Biz8080';
  
  
+ GRANT ALL privileges ON iolistDB.* TO 'korea'@'localhost';
  
  
+ drop table tbl_product;
  
+ CREATE TABLE tbl_iolist (
+	io_seq bigint      PRIMARY KEY AUTO_INCREMENT,
+	io_date varchar(10) NOT NULL,        
+	io_time varchar(10) NOT NULL,        
+	io_pcode    varchar(5)  NOT NULL,        
+	io_dcode    varchar(4)  NOT NULL,        
+	io_inout    varchar(1)  NOT NULL,        
+	io_qty  bigint  NOT NULL,        
+	io_iprice   bigint,
+	io_oprice   bigint
+);
+
+CREATE TABLE tbl_dept (
+	d_code  varchar(4)      PRIMARY KEY,
+	d_name  varchar(125)    NOT NULL,    
+	d_ceo   varchar(50) NOT NULL,    
+	d_tel   varchar(20) NOT NULL,    
+	d_address   varchar(125)    
+);
+
+CREATE TABLE tbl_product (
+	p_code  varchar(5)      PRIMARY KEY,
+	p_name  varchar(50) NOT NULL,    
+	p_iprice    bigint,      
+	p_oprice    bigint     
+);
+
+insert into tbl_user2 values( 'hong', '홍길동', '010-1111-1111');
+insert into tbl_user2 values( 'lee', '이몽룡', '010-2222-2222');
+insert into tbl_user2 values( 'leem', '임꺽정', '010-3333-3333');
+insert into tbl_user values( '0002', '우', 1000, 3000);
+insert into tbl_user values( '0003', '우우리', 1500, 3000);
  
+ SELECT * FROM tbl_user
+WHERE nickname = '이몽룡';
+
+DELETE FROM tbl_user
+WHERE username = 'hong';
+
+
+UPDATE tbl_user
+SET tel = '010-1111-5555'
+WHERE username ='hong';
+
+SELECT * FROM tbl_user
+ORDER BY username, nickname DESC;
+
+SELECT * FROM tbl_user
+WHERE nickname LIKE '%길%'; 
  
+ CREATE TABLE tbl_user2 (
+	username varchar(125) primary key,
+    nickname varchar(125) NOT null,
+    tel varchar(20)
+);
  
+select *
+from tbl_iolist
+    left join tbl_dept
+     ON io_dcode = d_code
+     WHERE d_name IS NULL;
+
+select *
+from tbl_iolist
+    left join tbl_product
+     ON io_pcode = p_code
+     WHERE p_name IS NULL;
+ 
+ ALTER TABLE tbl_iolist 
+ADD CONSTRAINT f_dept
+FOREIGN KEY (io_dcode)
+REFERENCES tbl_dept(d_code);
+
+ALTER TABLE tbl_iolist 
+ADD CONSTRAINT f_product
+FOREIGN KEY (io_pcode)
+REFERENCES tbl_dept(p_code);
+
+SELECT * FROM tbl_iolist
+WHERE io_date BETWEEN ‘2023-03-01’ AND ‘2023-03-31’;
+
+SELECT *
+FROM tbl_iolist
+WHERE io_inout = 1
+  AND io_qty >= 60
+  AND io_qty < 70;
+  
+  DESC tbl_product;
+  
+  CREATE VIEW view_iolist
+AS (
+SELECT io_seq, io_date, io_time, io_dcode AS 거래처코드, d_name AS 거래처명, 
+	 io_pcode AS 상품코드, p_name AS 상품명, io_inout, io_qty,
+	 io_iprice AS 매입단가, io_oprice AS 매출단가
+    FROM tbl_iolist
+        LEFT JOIN tbl_dept
+            ON io_dcode = d_code
+        LEFT JOIN tbl_product
+            ON io_pcode = p_code
+);
+
+drop view view_iolist;
+
+
+SELECT p_code AS 상품코드, p_name AS 상품명, sum(p_code) AS 레코드수
+FROM tbl_product
+GROUP BY p_code, p_name;
+
+SELECT io_inout AS 구분, p_code AS 상품코드, p_name AS 상품명, SUM(io_inout*io_qty) AS 수량합계
+FROM tbl_iolist, tbl_product
+WHERE io_inout = 1
+GROUP BY io_inout, p_code, p_name;
+
+SELECT
+    io_inout AS 구분,
+    tbl_iolist.p_code AS 상품코드,
+    tbl_product.p_name AS 상품명,
+    SUM(tbl_iolist.io_qty) AS 수량합계
+FROM
+    tbl_iolist
+JOIN
+    tbl_product ON tbl_iolist.p_code = tbl_product.p_code
+WHERE
+    io_inout = 1
+GROUP BY
+    io_inout, tbl_iolist.p_code, tbl_product.p_name;
+
+SELECT
+SUM(io_qty * io_iprice) AS 매입합계
+FROM tbl_iolist
+WHERE io_inout;
+
+
+io_inout AS 거래구분, io_iprice AS 매입합계, io_oprice AS 매출합계
